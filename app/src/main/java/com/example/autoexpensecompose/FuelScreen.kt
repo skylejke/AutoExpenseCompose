b39e7cf93934f84d433ffe388ea8c4cfeab23c34
Daniil <danilmega2003@gmail.com>
2023-10-21 20:17:09 +0300

    добавлен ворк манагер (потом удалю)

--- app/src/main/java/com/example/autoexpensecompose/FuelScreen.kt
+++ app/src/main/java/com/example/autoexpensecompose/FuelScreen.kt
@@ -2,17 +2,36 @@ package com.example.autoexpensecompose
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 import com.example.autoexpensecompose.ui.theme.AutoExpenseComposeTheme
+
 
 @Composable
 fun FuelScreen() {
+    val context = LocalContext.current
     Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-        Text(text = "Fuel Screen")
+        Button(onClick = {
+            val delaySeconds = 10
+            val inputData = Data.Builder().putInt("delaySeconds", delaySeconds).build()
+            val workRequest = OneTimeWorkRequest.Builder(MyWorkManager::class.java)
+                .setInputData(inputData)
+                .build()
+
+            WorkManager.getInstance(context).enqueue(workRequest)
+
+        }) {
+            Text(text = "Установить таймер")
+        }
+//        Text(text = "Fuel Screen")
     }
 }
 
